7bba52c12aedcc074d12aa5432b878e792c503c2
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2020-04-17 15:20:13 +0700

    update

---
+++ src/SelfCheckout/SelfCheckout.UWP/Services/NativeApiService.cs
@@ -0,0 +1,20 @@
+﻿using SelfCheckout.Services;
+using SelfCheckout.UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(NativeApiService))]
+namespace SelfCheckout.UWP.Services
+{
+    public class NativeApiService : INativeApiService
+    {
+        public void ExitApp()
+        {
+            App.Current.Exit();
+        }
+    }
+}
