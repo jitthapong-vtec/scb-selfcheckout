526706b8bf91ade586cc1d696030b30d5c311cf0
Jitthapong Arjsalee <jitthapong@vtec-system.com>
2020-05-28 13:57:52 +0700

    update

--- src/SelfCheckout/SelfCheckout/GlobalSettings.cs
+++ src/SelfCheckout/SelfCheckout/GlobalSettings.cs
@@ -66,7 +66,7 @@ namespace SelfCheckout
 
         public string PimCoreUrl { get => "https://pim.kingpower.com/"; }
 
-        public bool TestMode { get => true; }
+        public bool TestMode { get => false; }
 
         public string MachineIp
         {
